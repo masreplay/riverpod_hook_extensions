f9730c4b46868c62067d409c2f941dd2294d50cd
M Atheer <55885230+masreplay@users.noreply.github.com>
2024-04-20 03:06:56 +0300

    Refactor form_key_hook and platform.dart

--- lib/src/hooks/form_key_hook.dart
+++ lib/src/hooks/form_key_hook.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 GlobalKey<T> useGlobalKey<T extends State<StatefulWidget>>() {
-  return useMemoized(GlobalKey<T>.new, const []);
+  return useMemoized(GlobalKey<T>.new);
 }
 
 GlobalKey<FormState> useFormKey() {
-  return useMemoized(GlobalKey<FormState>.new, const []);
+  return useMemoized(GlobalKey<FormState>.new);
 }
--- lib/src/platform.dart
+++ lib/src/platform.dart
@@ -1,15 +1,22 @@
-sealed class PlatformData<Android, IOS> {
+sealed class PlatformData<Android, IOS, Web> {
   const PlatformData();
 }
 
-class PlatformAndroid<Android, IOS> extends PlatformData<Android, IOS> {
+class PlatformAndroid<Android, IOS, Web>
+    extends PlatformData<Android, IOS, Web> {
   final Android data;
 
   const PlatformAndroid(this.data);
 }
 
-class PlatformIOS<Android, IOS> extends PlatformData<Android, IOS> {
+class PlatformIOS<Android, IOS, Web> extends PlatformData<Android, IOS, Web> {
   final IOS data;
 
   const PlatformIOS(this.data);
+}
+
+class PlatformWeb<Android, IOS, Web> extends PlatformData<Android, IOS, Web> {
+  final Web data;
+
+  const PlatformWeb(this.data);
 }
