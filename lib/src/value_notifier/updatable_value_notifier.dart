2f5921e17f7c0867caf4f61f87f8f57950230f31
Mohammed Atheer <mohammed.atheer@ta3leem.dev>
2024-09-15 10:11:46 +0300

    Refactor maybeUpdate() method in UpdatableValueNotifier extension

--- lib/src/value_notifier/updatable_value_notifier.dart
+++ lib/src/value_notifier/updatable_value_notifier.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 extension UpdatableValueNotifier<T> on ValueNotifier<T> {
   void update(T newValue) => value = newValue;
 
-  void maybeUpdate(T? newValue) {
-    if (newValue != null) value = newValue;
+  void maybeUpdate(T? value) {
+    if (value != null) this.value = value;
   }
 }
